3ce4d81c5f437c5a97fda3df52aab3683eea034a
Anton Agestam <msn@antonagestam.se>
2013-05-15 17:00:34 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Collectfast',
-      version='0.0.0',
+      version='0.1.0',
       description='Custom management command that compares the MD5 sum and '
                   'etag from S3 and if the two are the same skips file copy.',
       author='Anton Agestam',
