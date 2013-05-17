acfa94a15e75310388a60b60c27e3fb915794444
Anton Agestam <msn@antonagestam.se>
2013-05-17 12:14:46 +0200

    let Django cache lookups

--- collectfast/management/commands/collectstatic.py
+++ collectfast/management/commands/collectstatic.py
@@ -1,8 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import with_statement
 import hashlib
 from optparse import make_option
 
 from django.contrib.staticfiles.management.commands import collectstatic
+from django.core.cache import cache
+
+from dateutil import parser
 
 class Command(collectstatic.Command):
     option_list = collectstatic.Command.option_list + (
@@ -11,9 +16,32 @@ class Command(collectstatic.Command):
             help="Upload the file even though the ETags match."),
     )
 
+    lookups = None
+
     def set_options(self, **options):
         self.ignore_etag = options.pop('ignore_etag', False)
         super(Command, self).set_options(**options)
+
+    def get_lookup(self, path):
+        """Get lookup from local dict, cache or S3 — in that order"""
+
+        if self.lookups is None:
+            self.lookups = {}
+
+        if path not in self.lookups:
+            cache_key = 'collectfast_asset_' + path
+            cached = cache.get(cache_key, False)
+
+            if cached is False:
+                self.lookups[path] = self.storage.bucket.lookup(path)
+                cache.set(cache_key, self.lookups[path])
+            else:
+                self.lookups[path] = cached
+
+        return self.lookups[path]
+
+    def get_last_modified(self, path):
+        return parser.parse(self.get_lookup(path).last_modified)
 
     def copy_file(self, path, prefixed_path, source_storage):
         """
@@ -25,7 +53,8 @@ class Command(collectstatic.Command):
         if not self.ignore_etag:
             try:
                 # Get ETag from storage
-                storage_etag = self.storage.bucket.lookup(prefixed_path).etag
+                storage_lookup = self.get_lookup(prefixed_path)
+                storage_etag = storage_lookup.etag
 
                 # Create md5 checksum from local file
                 file_contents = source_storage.open(path).read()
