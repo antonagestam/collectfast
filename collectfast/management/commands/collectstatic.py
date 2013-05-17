c57908f305a1f74558e611cddf2238827788b5e6
Anton Agestam <msn@antonagestam.se>
2013-05-17 13:03:19 +0200

    added caching of lookups and comparing last_modified before comparing ETags

--- collectfast/management/commands/collectstatic.py
+++ collectfast/management/commands/collectstatic.py
@@ -3,11 +3,13 @@
 from __future__ import with_statement
 import hashlib
 from optparse import make_option
+import datetime
 
 from django.contrib.staticfiles.management.commands import collectstatic
 from django.core.cache import cache
 
 from dateutil import parser
+import pytz
 
 class Command(collectstatic.Command):
     option_list = collectstatic.Command.option_list + (
@@ -22,6 +24,18 @@ class Command(collectstatic.Command):
         self.ignore_etag = options.pop('ignore_etag', False)
         super(Command, self).set_options(**options)
 
+    def collect(self, *args, **kwargs):
+        """Override collect method to track time"""
+
+        start = datetime.datetime.now()
+        ret = super(Command, self).collect(*args, **kwargs)
+        self.log("Collected static in " + str(datetime.datetime.now() - start),
+                 level=1)
+        return ret
+
+    def get_cache_key(self, path):
+        return 'collectfast_asset_' + hashlib.md5(path).hexdigest()
+
     def get_lookup(self, path):
         """Get lookup from local dict, cache or S3 — in that order"""
 
@@ -29,7 +43,7 @@ class Command(collectstatic.Command):
             self.lookups = {}
 
         if path not in self.lookups:
-            cache_key = 'collectfast_asset_' + path
+            cache_key = self.get_cache_key(path)
             cached = cache.get(cache_key, False)
 
             if cached is False:
@@ -40,8 +54,17 @@ class Command(collectstatic.Command):
 
         return self.lookups[path]
 
+    def destroy_lookup(self, path):
+        del self.lookups[path]
+        cache.delete(self.get_cache_key(path))
+
     def get_last_modified(self, path):
         return parser.parse(self.get_lookup(path).last_modified)
+
+    def get_local_modified(self, path, storage):
+        date = storage.modified_time(path)
+        utc = pytz.UTC
+        return utc.localize(date)
 
     def copy_file(self, path, prefixed_path, source_storage):
         """
@@ -52,21 +75,33 @@ class Command(collectstatic.Command):
 
         if not self.ignore_etag:
             try:
-                # Get ETag from storage
                 storage_lookup = self.get_lookup(prefixed_path)
-                storage_etag = storage_lookup.etag
+                local_file = source_storage.open(prefixed_path)
+                storage_modified = self.get_last_modified(prefixed_path)
+                local_modified = self.get_local_modified(prefixed_path,
+                                                         source_storage)
+
+                # Compare last_modified dates
+                if local_modified > storage_modified:
+                    self.log(u"Skipping '%s' due to last_modified" % path,
+                             level=1)
+                    return False
 
                 # Create md5 checksum from local file
-                file_contents = source_storage.open(path).read()
+                file_contents = local_file.read()
                 local_etag = '"%s"' % hashlib.md5(file_contents).hexdigest()
 
                 # Compare checksums and skip copying if matching
-                if storage_etag == local_etag:
-                    self.log(u"Skipping '%s'" % path, level=1)
+                if storage_lookup.etag == local_etag:
+                    self.log(u"Skipping '%s' due to ETag" % path, level=1)
                     return False
-            except:
+            except Exception, e:
                 # Ignore errors, let default Command handle it
-                pass
+                raise e
+                #pass
+
+            # Invalidate cached versions of lookup if copy is done
+            self.destroy_lookup(prefixed_path)
 
         return super(Command, self).copy_file(path, prefixed_path,
                                               source_storage)
