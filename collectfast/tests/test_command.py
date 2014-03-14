bb6e030b97a4bada97bae442730613ccee634541
Anton Agestam <msn@antonagestam.se>
2014-03-14 19:15:36 +0100

    wrote majority of tests

--- collectfast/tests/test_command.py
+++ collectfast/tests/test_command.py
@@ -22,56 +22,83 @@ To test:
     - Properly calls super
 """
 
-import sys
-from StringIO import StringIO
-from contextlib import contextmanager
 from unittest import TestCase
-from django.core.management import call_command
+from mock import patch
 
-from ..management.commands.collectstatic import Command
-
-
-@contextmanager
-def redirect_output(out=sys.stdout, err=sys.stderr):
-    """
-    Redirect stdout to variable, see http://stackoverflow.com/questions/13250050/redirecting-the-output-of-a-python-function-from-stdout-to-variable-in-python
-    """
-    saved = sys.stdout, sys.stderr
-    sys.stdout, sys.stderr = out, err
-    try:
-        yield
-    finally:
-        sys.stdout, sys.stderr = saved
+from ..management.commands.collectstatic import Command, cache
 
 
 class TestCommand(TestCase):
-    def call_command(self, *args, **kwargs):
-        out = StringIO()
-        err = StringIO()
-        with redirect_output(out, err):
-            out.flush()
-            err.flush()
-            call_command('collectstatic', interactive=False, *args, **kwargs)
-            output = out.getvalue()
-        return output
+    def setUp(self):
+        cache.clear()
 
     def get_command(self, *args, **kwargs):
         return Command(*args, **kwargs)
 
-    def test_collect(self):
+    @patch("collectfast.management.commands.collectstatic.collectstatic"
+           ".Command.collect")
+    def test_collect(self, mocked_super):
         command = self.get_command()
         command.collect()
         self.assertEqual(command.num_skipped_files, 0)
         self.assertIsInstance(command.collect_time, str)
 
     def test_get_cache_key(self):
-        pass
+        command = self.get_command()
+        cache_key = command.get_cache_key('/some/random/path')
+        prefix_len = len(command.cache_key_prefix)
+        self.assertTrue(cache_key.startswith(command.cache_key_prefix))
+        self.assertEqual(32 + prefix_len, len(cache_key))
+
+    @patch("collectfast.management.commands.collectstatic.cache.get")
+    @patch("collectfast.management.commands.collectstatic.Command"
+           ".get_storage_lookup")
+    def mock_get_lookup(self, path, cached_value, mocked_lookup, mocked_cache):
+        mocked_lookup.return_value = 'a fresh lookup'
+        mocked_cache.return_value = cached_value
+        command = self.get_command()
+        ret_val = command.get_lookup(path)
+        return ret_val, mocked_lookup, mocked_cache
+
+    def get_fresh_lookup(self, path):
+        return self.mock_get_lookup(path, False)
+
+    def get_cached_lookup(self, path):
+        return self.mock_get_lookup(path, 'a cached lookup')
 
     def test_get_lookup(self):
-        pass
+        path = '/some/unique/path'
+        cache_key = self.get_command().get_cache_key(path)
 
-    def test_destroy_lookup(self):
-        pass
+        # Assert storage lookup is hit and cache is populated
+        ret_val, mocked_lookup, mocked_cache = self.get_fresh_lookup(path)
+        mocked_lookup.assert_called_once_with(path)
+        self.assertEqual(ret_val, 'a fresh lookup')
+        self.assertEqual(cache.get(cache_key), 'a fresh lookup')
 
-    def test_copy_file(self):
-        pass
+        # Assert storage is not hit, but cache is
+        ret_val, mocked_lookup, mocked_cache = self.get_cached_lookup(path)
+        self.assertEqual(mocked_lookup.call_count, 0)
+        self.assertEqual(mocked_cache.call_count, 1)
+        self.assertEqual(ret_val, 'a cached lookup')
+
+    @patch("collectfast.management.commands.collectstatic.Command"
+           ".get_storage_lookup")
+    def test_destroy_lookup(self, mocked_lookup):
+        mocked_lookup.return_value = 'a fake lookup'
+        c = self.get_command()
+        path = '/another/unique/path'
+        cache_key = c.get_cache_key(path)
+        c.get_lookup(path)
+        self.assertEqual(cache.get(cache_key), mocked_lookup.return_value)
+        self.assertEqual(c.lookups[path], mocked_lookup.return_value)
+
+        c.destroy_lookup(path)
+        self.assertEqual(cache.get(cache_key, 'empty'), 'empty')
+        self.assertNotIn(path, c.lookups)
+
+    @patch("collectfast.management.commands.collectstatic.Command.get_lookup")
+    def test_copy_file(self, mocked_lookup):
+        c = self.get_command()
+        c.execute(interactive=False)
+        self.assertEqual(mocked_lookup.call_count, 1)
