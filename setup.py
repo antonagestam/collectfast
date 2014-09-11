48fbd47c252d3af568506f85bf77a87498c478be
Anton Agestam <msn@antonagestam.se>
2014-09-11 16:23:49 +0200

    updated dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 setup(
     name='Collectfast',
     description='Collectstatic on Steroids',
-    version='0.2.0',
+    version='0.2.1',
     long_description=open('README.md').read(),
     author='Anton Agestam',
     author_email='msn@antonagestam.se',
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/FundedByMe/collectfast/',
     license='Creative Commons Attribution-ShareAlike 3.0 Unported License',
     include_package_data=True,
-    install_requires=['Django>=1.4', 'python-dateutil>=2.1', 'pytz>=2013b',],
+    install_requires=['Django>=1.4', 'python-dateutil>=2.1', 'pytz>=2014.2', ],
     classifiers=['Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3']
 )
