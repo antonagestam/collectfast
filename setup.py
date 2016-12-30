3b194aba2c7eea9ccf156f79836e26cb96367657
Anton Agestam <msn@antonagestam.se>
2016-12-30 15:26:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 setup(
     name='Collectfast',
     description='A Faster Collectstatic',
-    version='0.4.0',
+    version='0.4.1',
     long_description=open('README.rst').read(),
     author='Anton Agestam',
     author_email='msn@antonagestam.se',
