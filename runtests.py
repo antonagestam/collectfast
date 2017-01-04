36a5b551873eb5191c007a4de9283202a8a4c96c
Anton Agestam <msn@antonagestam.se>
2017-01-04 18:56:55 +0100

    try fix permissions

--- runtests.py
+++ runtests.py
@@ -32,6 +32,7 @@ def main():
         os.makedirs(staticfiles_dir)
     if not os.path.exists(staticroot_dir):
         os.makedirs(staticroot_dir)
+        os.chmod(staticroot_dir, 0o777)
 
     settings.configure(**{
         "DATABASES": {
