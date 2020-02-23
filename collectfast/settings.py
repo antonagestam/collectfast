1865d6c4f62114ea46d621486bcc4e5681d641c1
Anton Agestam <git@antonagestam.se>
2020-02-23 03:01:48 +0100

    Validate types of settings

--- collectfast/settings.py
+++ collectfast/settings.py
@@ -1,21 +1,35 @@
-from typing import Sequence
+from typing import Container
+from typing import Type
+from typing import TypeVar
 
 from django.conf import settings
 from typing_extensions import Final
 
 
-debug: Final[bool] = getattr(
-    settings, "COLLECTFAST_DEBUG", getattr(settings, "DEBUG", False)
+T = TypeVar("T")
+
+
+def _get_setting(type_: Type[T], key: str, default: T) -> T:
+    value = getattr(settings, key, default)
+    if not isinstance(value, type_):
+        raise ValueError(
+            f"The {key!r} setting must be of type {type_!r}, found {type(value)}"
+        )
+    return value
+
+
+debug: Final = _get_setting(
+    bool, "COLLECTFAST_DEBUG", _get_setting(bool, "DEBUG", False)
 )
-cache_key_prefix: Final[str] = getattr(
-    settings, "COLLECTFAST_CACHE_KEY_PREFIX", "collectfast06_asset_"
+cache_key_prefix: Final = _get_setting(
+    str, "COLLECTFAST_CACHE_KEY_PREFIX", "collectfast06_asset_"
 )
-cache: Final[str] = getattr(settings, "COLLECTFAST_CACHE", "default")
-threads: Final[bool] = getattr(settings, "COLLECTFAST_THREADS", False)
-enabled: Final[bool] = getattr(settings, "COLLECTFAST_ENABLED", True)
-aws_is_gzipped: Final[bool] = getattr(settings, "AWS_IS_GZIPPED", False)
-gzip_content_types: Final[Sequence[str]] = getattr(
-    settings,
+cache: Final = _get_setting(str, "COLLECTFAST_CACHE", "default")
+threads: Final = _get_setting(bool, "COLLECTFAST_THREADS", False)
+enabled: Final = _get_setting(bool, "COLLECTFAST_ENABLED", True)
+aws_is_gzipped: Final = _get_setting(bool, "AWS_IS_GZIPPED", False)
+gzip_content_types: Final[Container] = _get_setting(
+    tuple,
     "GZIP_CONTENT_TYPES",
     (
         "text/css",
