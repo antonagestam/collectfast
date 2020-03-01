ae03daf0ac8ad7ac977b1127da8b354e52cd2717
Anton Agestam <git@antonagestam.se>
2020-03-01 21:41:52 +0100

    Support post_process()

--- collectfast/management/commands/collectstatic.py
+++ collectfast/management/commands/collectstatic.py
@@ -18,6 +18,7 @@ from collectfast.strategies import DisabledStrategy
 from collectfast.strategies import load_strategy
 from collectfast.strategies import Strategy
 
+
 Task = Tuple[str, str, Storage]
 
 
@@ -28,6 +29,7 @@ class Command(collectstatic.Command):
         self.tasks: List[Task] = []
         self.collectfast_enabled = settings.enabled
         self.strategy: Strategy = DisabledStrategy(Storage())
+        self.found_files: Dict[str, Tuple[Storage, str]] = {}
 
     @staticmethod
     def _load_strategy() -> Type[Strategy[Storage]]:
@@ -53,7 +55,6 @@ class Command(collectstatic.Command):
         )
 
     def set_options(self, **options: Any) -> None:
-        """Set options and handle deprecation."""
         self.collectfast_enabled = self.collectfast_enabled and not options.pop(
             "disable_collectfast"
         )
@@ -66,13 +67,24 @@ class Command(collectstatic.Command):
         Override collect to copy files concurrently. The tasks are populated by
         Command.copy_file() which is called by super().collect().
         """
-        ret = super().collect()
-        if not self.collectfast_enabled:
-            return ret
-        if settings.threads:
-            with ThreadPoolExecutor(settings.threads) as pool:
-                pool.map(self.maybe_copy_file, self.tasks)
-        return ret
+        if not self.collectfast_enabled or not settings.threads:
+            return super().collect()
+
+        # Store original value of post_process in super_post_process and always
+        # set the value to False to prevent the default behavior from
+        # interfering when using threads. See maybe_post_process().
+        super_post_process = self.post_process
+        self.post_process = False
+
+        return_value = super().collect()
+
+        with ThreadPoolExecutor(settings.threads) as pool:
+            pool.map(self.maybe_copy_file, self.tasks)
+
+        self.maybe_post_process(super_post_process)
+        return_value["post_processed"] = self.post_processed_files
+
+        return return_value
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         """Override handle to suppress summary output."""
@@ -85,6 +97,11 @@ class Command(collectstatic.Command):
     def maybe_copy_file(self, args: Task) -> None:
         """Determine if file should be copied or not and handle exceptions."""
         path, prefixed_path, source_storage = args
+
+        # Build up found_files to look identical to how it's created in the
+        # builtin command's collect() method so that we can run post_process
+        # after all parallel uploads finish.
+        self.found_files[prefixed_path] = (source_storage, path)
 
         if self.collectfast_enabled and not self.dry_run:
             self.strategy.pre_should_copy_hook()
@@ -126,3 +143,29 @@ class Command(collectstatic.Command):
             pass
 
         return True
+
+    def maybe_post_process(self, super_post_process: bool) -> None:
+        # This method is extracted and modified from the collect() method of the
+        # builtin collectstatic command.
+        # https://github.com/django/django/blob/5320ba98f3d253afcaa76b4b388a8982f87d4f1a/django/contrib/staticfiles/management/commands/collectstatic.py#L124
+
+        if not super_post_process or not hasattr(self.storage, "post_process"):
+            return
+
+        processor = self.storage.post_process(self.found_files, dry_run=self.dry_run)
+
+        for original_path, processed_path, processed in processor:
+            if isinstance(processed, Exception):
+                self.stderr.write("Post-processing '%s' failed!" % original_path)
+                # Add a blank line before the traceback, otherwise it's
+                # too easy to miss the relevant part of the error message.
+                self.stderr.write("")
+                raise processed
+            if processed:
+                self.log(
+                    "Post-processed '%s' as '%s'" % (original_path, processed_path),
+                    level=2,
+                )
+                self.post_processed_files.append(original_path)
+            else:
+                self.log("Skipped post-processing '%s'" % original_path)
