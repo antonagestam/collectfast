27812646225a89c5e596706f66c4ab2fe4ddfd36
Gordon Pendleton <wgordonw1@gmail.com>
2020-05-08 13:27:39 -0400

    fixed reference

--- collectfast/tests/command/test_post_copy_hook.py
+++ collectfast/tests/command/test_post_copy_hook.py
@@ -52,7 +52,7 @@ def test_calls_post_copy_hook_true(case: TestCase) -> None:
     cmd.strategy.post_copy_hook.assert_called_once_with(
         mock.ANY,
         mock.ANY,
-        self.remote_storage,
+        cmd.strategy.remote_storage,
         True,
     )
 
